83194ec9063cc924e623440b6dc51c23b4d9948c
chrisluo1001 <59398283+chrisluo1001@users.noreply.github.com>
2020-08-26 21:57:55 -0400

    Update smart-app.js

--- src/js/smart-app.js
+++ src/js/smart-app.js
@@ -31,7 +31,11 @@
       }
     }
 
-    FHIR.oauth2.ready(onReady, onError);
+    //FHIR.oauth2.ready(onReady, onError);
+    FHIR.oauth2.ready()
+    .then(client => client.request("Patient"))
+    .then(console.log)
+    .catch(console.error);
     return ret.promise();
 
   };
