50b942aa564e77cf1349a79ed5c67925bea33484
chrisluo1001 <59398283+chrisluo1001@users.noreply.github.com>
2020-08-29 17:57:49 -0400

    Update smart-app.js

--- src/js/smart-app.js
+++ src/js/smart-app.js
@@ -8,25 +8,29 @@
     }
 
     function onReady(smart)  {
-      console.log(smart.userId);
-      console.log(smart.tokenResponse.access_token);
-      var settings = {
-          "async": true,
-          //"url": userId,
-          "url": "https://fhir-ehr-code.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient/12724069",
-          "method": "GET",
-          "headers": {
-              "Content-Type": "application/json",
-              "Accept": "application/json",
-              "Authorization": "Bearer " + smart.tokenResponse.access_token
-          },
-      }
+      if (smart.hasOwnProperty('userId')) {
+        console.log(smart.userId);
+        console.log(smart.tokenResponse.access_token);
+        var settings = {
+            "async": true,
+            //"url": userId,
+            "url": "https://fhir-ehr-code.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient/12724069",
+            "method": "GET",
+            "headers": {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Bearer " + smart.tokenResponse.access_token
+            },
+        }
 
-      $.ajax(settings).done(function (response) {
-          console.log("prationer ajax call ");
-          console.log(response);
-          alert(JSON.stringify(response));
-      })
+        $.ajax(settings).done(function (response) {
+            console.log("prationer ajax call ");
+            console.log(response);
+            alert(JSON.stringify(response));
+        })
+      } else {
+        onError();
+      }
       
       if (smart.hasOwnProperty('patient')) {
         //alert('onReady');
